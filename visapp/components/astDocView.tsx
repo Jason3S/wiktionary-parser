29f9a5ebe8a7acbdfced73de453402d8bd2f08d2
Jason Dent <jason@revinate.com>
2016-10-27 21:09:52 +0200

    Last bits

--- visapp/components/astDocView.tsx
+++ visapp/components/astDocView.tsx
@@ -3,6 +3,14 @@
 // Required for JSX to compile
 import React = require('react');
 /* tslint:enable:no-unused-variable */
+import createFragment = require('react-addons-create-fragment');
+
+
+type Children = JSX.Element | JSX.Element[];
+
+interface BaseProps {
+    children?: Children;
+}
 
 interface AstViewProps extends AstProps {}
 
@@ -12,8 +20,37 @@ interface ModelProperties extends AstViewProps {
 }
 
 
+type RenderFunction = (props: AstViewProps) => React.ReactNode;
 
-function RenderNode(props: AstViewProps) {
+interface RenderDictionary {
+    [index: string]: RenderFunction;
+}
+
+const renderFunctions: RenderDictionary = {
+};
+
+type RenderResultSingle = React.ReactElement<any> | string | boolean | number;
+type RenderResult = RenderResultSingle | RenderResultSingle[];
+
+function defaultRenderFn(props: AstViewProps): React.ReactNode {
+    const { model, query } = props;
+    if (model.v !== undefined) {
+        return (<span key={model.id}>{model.v}</span>);
+    }
+
+    const children = (model.c || []).map(m => m ? RenderNode({query, model: m}) : null);
+
+    return (
+        <div key={model.id} className={'ast-' + model.t} >
+            <b><i>{model.t}</i></b>
+            <div>
+                {children}
+            </div>
+        </div>
+    );
+}
+
+function RenderNode(props: AstViewProps): JSX.Element {
 
     const { model, query } = props;
     if (model.v !== undefined) {
@@ -42,3 +79,10 @@ function AstDocView(props: AstProps) {
 }
 
 export { AstDocView };
+
+function NodeText(props: BaseProps): JSX.Element {
+    return props.children
+        ? (<div style="display: inline-block">{props.children}</div>)
+        : null;
+}
+
